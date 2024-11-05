5e9a1f94278521490127f790a9c1868973da2098
JiheonSong <jhsong.iphone@gmail.com>
2024-11-05 17:04:47 +0900

    Update: useState and useRef example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
+import { useRef, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+
+  const increaseCountState = () => {
+    setCount((count) => count + 1);
+    // 랜더링 됨
+  };
+
+  const increaseCountRef = () => {
+    countRef.current = countRef.current + 1;
+    // 랜더링 안됨
+  };
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/hello">
-          <h1>Hello</h1>
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <p>State: {count}</p>
+      <p>Ref: {countRef.current}</p>
+      <button onClick={increaseCountState}>State Up</button>
+      <button onClick={increaseCountRef}>Ref Up</button>
+    </div>
   );
 }
 
